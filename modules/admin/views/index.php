3a9a11b9509471bc0be701bb20183ce9568b376c
v_ransu(苏冉) <v_ransu@tencent.com>
2017-11-30 11:13:38 +0800

    前端

--- modules/admin/views/index.php
+++ modules/admin/views/index.php
@@ -1,92 +1,57 @@
+<div class="layui-body">
 
-<div class="bread">
-文章列表
-</div>
-<div class="info-area">
-	<a href="/zhuanti/zhuanti/add" class="btn btn-info"><i class="fa fa-plus"></i> 添加文章</a>
-	<a href="/zhuanti/zhuanti/add?ctype=link" class="btn btn-info"><i class="fa fa-plus"></i> 添加外链</a>
-	<a href="/zhuanti/zhuanti/typelist" class="btn btn-info"><i class="fa fa-list-ul"></i> 专题列表</a>
-</div>
+    <fieldset class="layui-elem-field layui-field-title" style="margin-top: 50px;">
+        <legend>首页面</legend>
+    </fieldset>
 
-<div class="content">
-    <form action="" method="post" id="good-class-index" accept-charset="UTF-8">
-            <div class="index-content">
-                <div class="list-content">
-                    <div class="list-title">
-                        <img src="">
-                        <h3>纪律规定</h3>
-                    </div>
 
-                    <div class="list-content-item">
-                        <div class="list-item">
-                            <img src="">
-                            <h4 class="list-item-name"><a href="">规则设置</a></h4>
-                        </div>
+    <div class="layui-col-md3">
+        <div class="grid-demo grid-demo-bg1 boxdiv box1">
 
-                        <div class="list-item">
-                            <img src="" >
-                            <h4 class="list-item-name"><a href="">人员设置</a></h4>
-                        </div>
-
-                        <div class="list-item">
-                            <img src="" >
-                            <h4 class="list-item-name"><a href="">纪律教育</a></h4>
-                        </div>
-
-                    </div>
-                </div>
-
-                <div class="list-content">
-                    <div class="list-title">
-                        <img src="">
-                        <h3>检查登记</h3>
-                    </div>
-
-                    <div class="list-content-item">
-                        <div class="list-item">
-                            <img src="">
-                            <h4 class="list-item-name"><a href="">移动录入</a></h4>
-                        </div>
-
-                        <div class="list-item">
-                            <img src="" >
-                            <h4 class="list-item-name"><a href="">网页录入</a></h4>
-                        </div>
-
-                        <div class="list-item">
-                            <img src="" >
-                            <h4 class="list-item-name"><a href="">批量导入</a></h4>
-                        </div>
-
-                        <div class="list-item">
-                            <img src="" >
-                            <h4 class="list-item-name"><a href="">记录查询</a></h4>
-                        </div>
-
-                        <div class="list-item">
-                            <img src="" >
-                            <h4 class="list-item-name"><a href="">处分管理</a></h4>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="list-content">
-                    <div class="list-title">
-                        <img src="">
-                        <h3>评比分析</h3>
-                    </div>
-
-                    <div class="list-content-item">
-                        <div class="list-item">
-                            <img src="" >
-                            <h4 class="list-item-name"><a href="">日报周报</a></h4>
-                        </div>
-
-                        <div class="list-item">
-                            <img src="">
-                            <h4 class="list-item-name"><a href="">文明班评比</a></h4>
-                        </div>
-                    </div>
-                </div>
+            <div id="left-sidebar">
+                <h2>纪律规定</h2>
+                <ul>
+                    <li><a href="">规则设置</a></li>
+                    <li><a href="">人员设置</a></li>
+                    <li><a href="">纪律教育</a></li>
+                </ul>
             </div>
+
+        </div>
+    </div>
+
+    <div class="layui-col-md3">
+        <div class="grid-demo boxdiv box1">
+            <div id="left-sidebar">
+                <h2>检查登记</h2>
+                <ul>
+                    <li><a href="">移动录入</a></li>
+                    <li><a href="">网页录入</a></li>
+                    <li><a href="">批量导入</a></li>
+                    <li><a href="">记录查询</a></li>
+                    <li><a href="">处分管理</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <div class="layui-col-md3">
+        <div class="grid-demo grid-demo-bg1 boxdiv box1">
+            <div id="left-sidebar">
+                <h2>屏蔽分析</h2>
+                <ul>
+                    <li><a href="">日报周报</a></li>
+                    <li><a href="">文明班评比</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <div class="layui-col-md3">
+        <div class="grid-demo"></div>
+    </div>
+
 </div>
+
+
+
